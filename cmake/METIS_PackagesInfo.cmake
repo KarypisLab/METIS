0855b1448e48943f5d91dc390900deb094c9f491
Cristian Le <cristian.le@mpsd.mpg.de>
2023-10-26 17:12:58 +0200

    CMake project modernization
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

---
+++ cmake/METIS_PackagesInfo.cmake
@@ -0,0 +1,6 @@
+set_package_properties(GKlib PROPERTIES
+		URL https://github.com/KarypisLab/GKlib
+		DESCRIPTION "A library of various helper routines and frameworks used by many of the KarypisLab's software"
+		TYPE REQUIRED
+		PURPOSE "Utilties library"
+)
